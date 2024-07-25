1d67ade43e39a91352d429a7ded62660d2403fd4
dangminh214 <dangminh214@gmail.com>
2024-07-25 21:06:11 +0200

    add state for section

---
+++ src/components/Overview/Overview.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Overview = () => {
+  return (
+    <>
+      {" "}
+      <div>this is content of overview section</div>
+    </>
+  );
+};
--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,56 +1,80 @@
 import { useState } from "react";
+import { Overview } from "../Overview/Overview";
 
 export const Quote = () => {
-  const [sectionName, setSectionName] = useState(
-    "Welcome to my portfolio, let's find out who I am"
-  );
+  const defaultTitle = "Welcome to my portfolio";
+  const [sectionName, setSectionName] = useState(defaultTitle);
+
+  const [activated, setActivated] = useState(false);
+
+  const handleOverview = () => {
+    setActivated(true);
+    setSectionName("Overview");
+  };
+
+  const handleWorkingExprience = () => {
+    setActivated(true);
+    setSectionName("Working Experiences");
+  };
+
+  const handleMyProjects = () => {
+    setActivated(true);
+    setSectionName("My Projects");
+  };
+
+  const handleMyHardskills = () => {
+    setActivated(true);
+    setSectionName("My Hardskills");
+  };
+
+  const handleMyCertificates = () => {
+    setActivated(true);
+    setSectionName("My Certificates");
+  };
+
+  const handleBackButton = () => {
+    setActivated(false);
+    setSectionName(defaultTitle);
+  };
   return (
     <div>
       <p>{sectionName}</p>
-      <p>
-        {" "}
-        <button
-          className="section-button"
-          onClick={() => setSectionName("Overview")}
-        >
-          Overview about me
-        </button>
-      </p>
-      <p>
-        {" "}
-        <button
-          className="section-button"
-          onClick={() => setSectionName("My Working Experiences")}
-        >
-          My Working Experiences
-        </button>
-      </p>
-      <p>
-        {" "}
-        <button
-          className="section-button"
-          onClick={() => setSectionName("My Projects")}
-        >
-          My Projects
-        </button>
-      </p>
-      <p>
-        {" "}
-        <button
-          className="section-button"
-          onClick={() => setSectionName("My Hardskills")}
-        >
-          My Hardskills
-        </button>
-      </p>
-      <p>
-        <button
-          className="section-button"
-          onClick={() => setSectionName("My Certificates")}
-        >
-          My Certificates
-        </button>
-      </p>
+      {sectionName === "Overview" ? <Overview /> : ""}
+      {activated ? (
+        <button onClick={handleBackButton}>Back</button>
+      ) : (
+        <p>
+          <p>
+            {" "}
+            <button className="section-button" onClick={handleOverview}>
+              Overview about me
+            </button>
+          </p>
+          <p>
+            {" "}
+            <button className="section-button" onClick={handleWorkingExprience}>
+              My Working Experiences
+            </button>
+          </p>
+          <p>
+            {" "}
+            <button className="section-button" onClick={handleMyProjects}>
+              My Projects
+            </button>
+          </p>
+          <p>
+            {" "}
+            <button className="section-button" onClick={handleMyHardskills}>
+              My Hardskills
+            </button>
+          </p>
+          <p>
+            <button className="section-button" onClick={handleMyCertificates}>
+              My Certificates
+            </button>
+          </p>
+        </p>
+      )}
     </div>
   );
 };
