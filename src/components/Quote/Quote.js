b45db2832b1d953cadfa2e62475caa85f0c0bd67
dangminh214 <dangminh214@gmail.com>
2024-07-25 20:36:21 +0200

    adjust css button section

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 export const Quote = () => {
   const [sectionName, setSectionName] = useState(
     "Welcome to my portfolio, let's find out who I am"
@@ -6,19 +7,50 @@ export const Quote = () => {
   return (
     <div>
       <p>{sectionName}</p>
-      <button onClick={() => setSectionName("Overview")}>
-        Overview about me
-      </button>
-      <button onClick={() => setSectionName("My Working Experience")}>
-        My Working Experience
-      </button>
-      <button onClick={() => setSectionName("My Projects")}>My Projects</button>
-      <button onClick={() => setSectionName("My Hardskills")}>
-        My Hardskills
-      </button>
-      <button onClick={() => setSectionName("My Certificates")}>
-        My Certificates
-      </button>
+      <p>
+        {" "}
+        <button
+          className="section-button"
+          onClick={() => setSectionName("Overview")}
+        >
+          Overview about me
+        </button>
+      </p>
+      <p>
+        {" "}
+        <button
+          className="section-button"
+          onClick={() => setSectionName("My Working Experiences")}
+        >
+          My Working Experiences
+        </button>
+      </p>
+      <p>
+        {" "}
+        <button
+          className="section-button"
+          onClick={() => setSectionName("My Projects")}
+        >
+          My Projects
+        </button>
+      </p>
+      <p>
+        {" "}
+        <button
+          className="section-button"
+          onClick={() => setSectionName("My Hardskills")}
+        >
+          My Hardskills
+        </button>
+      </p>
+      <p>
+        <button
+          className="section-button"
+          onClick={() => setSectionName("My Certificates")}
+        >
+          My Certificates
+        </button>
+      </p>
     </div>
   );
 };
