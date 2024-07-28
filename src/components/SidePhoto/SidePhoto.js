699cc8dda17563fcd7ddb8460fa21ec945b889c4
dangminh214 <dangminh214@gmail.com>
2024-07-28 22:44:46 +0200

    add photos as Viewerpage for all sections

--- src/components/SidePhoto/SidePhoto.js
+++ src/components/SidePhoto/SidePhoto.js
@@ -1,22 +1,43 @@
 import React from "react";
-import imageSource from "../../assets/logo.png";
+import imageSource from "../../assets/logo.webp";
 import WorkingExperiencesImage from "../../assets/htv_logo.png";
-// Import other images if needed
+import JavascriptLogo from "../../assets/JavaScript-logo.png";
+import avatar from "../../assets/avatar.png";
+import hda from "../../assets/hda.png";
+import { Viewpager } from "./Viewpager/Viewpager";
 
 export const SidePhoto = ({ selectedSection }) => {
-  let image;
+  let images;
 
   switch (selectedSection) {
     case "Overview":
-      image = imageSource;
+      images = [avatar, hda];
       break;
     case "Working Experiences":
-      image = WorkingExperiencesImage;
+      images = [WorkingExperiencesImage, JavascriptLogo];
       break;
-    // Add other cases for different sections
+    case "My Projects":
+      images = [
+        "https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      ];
+      break;
+    case "My Skills":
+      images = [
+        "https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      ];
+      break;
+    case "My Certificates":
+      images = [
+        "https://images.pexels.com/photos/4016596/pexels-photo-4016596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      ];
+      break;
     default:
-      image = imageSource;
+      images = [imageSource]; // Default single image in an array
   }
 
-  return <img className="section-picture" src={image} alt="side-images" />;
+  return <Viewpager images={images} />;
 };
