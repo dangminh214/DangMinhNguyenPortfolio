795f55ca2d6366a5ee44a845ef483254490b972e
dangminh214 <dangminh214@gmail.com>
2024-07-28 19:31:51 +0200

    adjust css

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -4,41 +4,52 @@ import { Overview } from "../Overview/Overview";
 export const Quote = () => {
   const defaultTitle = "Welcome to my portfolio";
   const [sectionName, setSectionName] = useState(defaultTitle);
-
+  const [changedSection, setChangedSection] = useState(false);
   const [activated, setActivated] = useState(false);
 
   const handleOverview = () => {
     setActivated(true);
     setSectionName("Overview");
+    setChangedSection(true);
   };
 
   const handleWorkingExprience = () => {
     setActivated(true);
     setSectionName("Working Experiences");
+    setChangedSection(true);
   };
 
   const handleMyProjects = () => {
     setActivated(true);
     setSectionName("My Projects");
+    setChangedSection(true);
   };
 
   const handleMyHardskills = () => {
     setActivated(true);
     setSectionName("My Hardskills");
+    setChangedSection(true);
   };
 
   const handleMyCertificates = () => {
     setActivated(true);
     setSectionName("My Certificates");
+    setChangedSection(true);
   };
 
   const handleBackButton = () => {
     setActivated(false);
     setSectionName(defaultTitle);
+    setChangedSection(false);
   };
+
+  let styles = { color: "#1c1c84" };
+  if (!changedSection) {
+    styles = { color: "black" };
+  }
   return (
     <div>
-      <p>{sectionName}</p>
+      <h2 style={styles}>{sectionName}</h2>
       {sectionName === "Overview" ? <Overview /> : ""}
       {activated ? (
         <button onClick={handleBackButton}>Back</button>
