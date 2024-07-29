fd68da2544e8eefd78ffe227c6cc5760ea8f88bb
dangminh214 <dangminh214@gmail.com>
2024-07-29 04:02:31 +0200

    add responsive design and some images to projects

--- src/components/SidePhoto/SidePhoto.js
+++ src/components/SidePhoto/SidePhoto.js
@@ -4,6 +4,8 @@ import WorkingExperiencesImage from "../../assets/htv_logo.png";
 import JavascriptLogo from "../../assets/JavaScript-logo.png";
 import avatar from "../../assets/avatar.png";
 import hda from "../../assets/hda.png";
+import steamFlipCalcImg from "../../assets/steamflipcal.png";
+import steamFlipCalcImg2 from "../../assets/steamflipcalc2.png";
 import { Viewpager } from "./Viewpager/Viewpager";
 
 export const SidePhoto = ({ selectedSection }) => {
@@ -23,16 +25,16 @@ export const SidePhoto = ({ selectedSection }) => {
         break;
       case "My Projects":
         setImages([
-          "https://images.pexels.com/photos/3194519/pexels-photo-3194519.jpeg",
-          "https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-          "https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          steamFlipCalcImg,
+          steamFlipCalcImg2,
+          "https://static-00.iconduck.com/assets.00/typescript-plain-icon-256x256-ypojgpyj.png",
         ]);
         break;
       case "My Skills":
         setImages([
+          "https://media.licdn.com/dms/image/D4E12AQGmeG3bDuWOgA/article-cover_image-shrink_720_1280/0/1688282331421?e=2147483647&v=beta&t=5DAKzEUi8dK_d1ONVDqgUYnTRQmighfwqBiO2Pt0I7w",
           "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          "https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-          "https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          "https://static-00.iconduck.com/assets.00/typescript-icon-icon-1024x1024-vh3pfez8.png",
         ]);
         break;
       case "My Certificates":
