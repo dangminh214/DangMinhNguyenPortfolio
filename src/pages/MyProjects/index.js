d9b05db6ea9d7a039414a98b5423443bb8ae0363
dangminh214 <dangminh214@gmail.com>
2024-07-31 00:59:41 +0200

    add on change event

--- src/pages/MyProjects/index.js
+++ src/pages/MyProjects/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyProjects.css";
 
 export const MyProjects = () => {
+  const [taskValue, setTaskValue] = useState("");
+
   return (
     <div>
-      <p>These are my Projects</p>
+      <section className="addtask">
+        <form>
+          <input
+            className="large-input"
+            onChange={(e) => setTaskValue(e.target.value)}
+            type="text"
+            name="task"
+            id="task"
+            placeholder="Task Name"
+            autoComplete="off"
+            value={taskValue}
+          />
+          <button type="submit"> Add Task </button>
+          <span className="reset" onClick={() => setTaskValue("")}>
+            Reset
+          </span>
+        </form>
+      </section>
     </div>
   );
 };
